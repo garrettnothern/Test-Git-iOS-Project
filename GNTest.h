b6175de5291a500781ca9211abfd8817292581d1
Garrett Nothern <garrettnothern@Deidres-MacBook-Pro.local>
2014-04-24 16:23:04 -0700

    added a test class

---
+++ GNTest.h
@@ -0,0 +1,13 @@
+//
+//  GNTest.h
+//  Git Test
+//
+//  Created by Garrett Nothern on 4/24/14.
+//  Copyright (c) 2014 Garrett Nothern. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNTest : NSObject
+
+@end
---
+++ GNTest.m
@@ -0,0 +1,13 @@
+//
+//  GNTest.m
+//  Git Test
+//
+//  Created by Garrett Nothern on 4/24/14.
+//  Copyright (c) 2014 Garrett Nothern. All rights reserved.
+//
+
+#import "GNTest.h"
+
+@implementation GNTest
+
+@end
